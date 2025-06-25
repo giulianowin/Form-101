a5db322495113b4fc95ad46c905aae7c6afca661
giulianowin <homeservicesolutionsbygs@gmail.com>
2025-06-25 22:06:41 +0100

    Updated AppBackground.tsx

--- src/components/AppBackground.tsx
+++ src/components/AppBackground.tsx
@@ -11,7 +11,6 @@ const AppBackground: React.FC<AppBackgroundProps> = ({ children }) => {
       <div className="absolute inset-0 overflow-hidden opacity-30">
         <div className="absolute -top-40 -right-40 w-80 h-80 bg-purple-500 rounded-full"></div>
         <div className="absolute -bottom-40 -left-40 w-80 h-80 bg-blue-500 rounded-full"></div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-pink-500 rounded-full opacity-50"></div>
       </div>
 
       {/* Content */}
