7fbfcfcdaff8bd5ac74e42e06ffa9cebbe56dbb6
giulianowin <homeservicesolutionsbygs@gmail.com>
2025-06-25 21:54:27 +0100

    Remove blur and blend effects from background orbs

--- src/components/AppBackground.tsx
+++ src/components/AppBackground.tsx
@@ -9,9 +9,9 @@ const AppBackground: React.FC<AppBackgroundProps> = ({ children }) => {
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 relative">
       {/* Animated background elements */}
       <div className="absolute inset-0 overflow-hidden opacity-30">
-        <div className="absolute -top-40 -right-40 w-80 h-80 bg-purple-500 rounded-full mix-blend-multiply filter blur-xl"></div>
-        <div className="absolute -bottom-40 -left-40 w-80 h-80 bg-blue-500 rounded-full mix-blend-multiply filter blur-xl"></div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-pink-500 rounded-full mix-blend-multiply filter blur-xl opacity-50"></div>
+        <div className="absolute -top-40 -right-40 w-80 h-80 bg-purple-500 rounded-full"></div>
+        <div className="absolute -bottom-40 -left-40 w-80 h-80 bg-blue-500 rounded-full"></div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-pink-500 rounded-full opacity-50"></div>
       </div>
 
       {/* Content */}
